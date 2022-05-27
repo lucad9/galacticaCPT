1c477f26b497e6ee15ba7155293ef7947a7b2b58
violorichard <100855141+violorichard@users.noreply.github.com>
2022-05-27 17:00:08 -0400

    Update musicPlayer.java

--- musicPlayer.java
+++ musicPlayer.java
@@ -1,3 +1,4 @@
+//Imports libraries for playing audio and inputting files.
 import java.io.File;
 import java.io.IOException;
 import javax.sound.sampled.AudioFormat;
@@ -7,25 +8,53 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
+
+//Class that implements Runnable in order to run in the background.
 public class musicPlayer implements Runnable{
 
+    //String that will contain that requested music selection.
+    private String fileLocation = "null";
 
+    //Thread for pausing and looping the music.
     Thread t;
 
-    musicPlayer()
+    //Constructor method.
+    public musicPlayer() {
+    }
+
+    //Initializes the file path of the music and the thread.
+    public void play(String name)
     {
+        fileLocation = name;
         t = new Thread(this);
-        t.start(); // Starting the thread
+        t.start();
+    }
+
+    //Method that will run in the background.
+    @Override
+    public void run()
+    {
+        //Loops the audio.
+        while(true) {
+            //Calls the play sound method to play music in the background.
+            playSound(fileLocation);
+        }
     }
 
     // size of the byte buffer used to read/write the audio stream
     private static final int BUFFER_SIZE = 4096;
 
-    //Play a given audio file.
-    void play(String audioFilePath) {
 
-        File audioFile = new File(audioFilePath);
-        while (true) {
+
+        //Play a given audio file.
+        void playSound(String fileName){
+
+            //Creates a file that has the requested audio.
+            File audioFile = new File(fileName);
+            fileLocation = fileName;
+
+            //Obtains the audio, formats it, opens a data line  and starts the data line.
+            //Then reads the bytes playing from the audio and if the bytes stop coming in, it closes and drains the audio line.
             try {
                 AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
 
@@ -51,6 +80,7 @@ public class musicPlayer implements Runnable{
                 audioLine.stop();
                 audioLine.close();
 
+            //Error Messages.
             } catch (UnsupportedAudioFileException ex) {
                 System.out.println("The specified audio file is not supported.");
                 ex.printStackTrace();
@@ -62,9 +92,10 @@ public class musicPlayer implements Runnable{
                 ex.printStackTrace();
             }
         }
-    }
 
-    @Override
-    public void run() {
+    // for stopping the thread
+    public void stop()
+    {
+        t.suspend();
     }
 }
